a0d50d2ac7c9db4c3029aa11e8f77d5df0459e0c
Christophe Lachance <lachance.christophe@cgmatane.qc.ca>
2019-09-12 17:38:11 -0400

    Starts in Matane

--- app/src/main/java/ca/qc/cgmatane/gestionrdv/MapsActivity.java
+++ app/src/main/java/ca/qc/cgmatane/gestionrdv/MapsActivity.java
@@ -39,8 +39,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        LatLng matane = new LatLng(48.849, -67.528);
+        mMap.addMarker(new MarkerOptions().position(matane).title("Marker in Matane"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(matane));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(matane, 12f));
     }
 }
