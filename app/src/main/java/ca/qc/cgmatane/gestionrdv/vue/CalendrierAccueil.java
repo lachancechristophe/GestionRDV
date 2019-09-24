7c73e47b736b07901ff84638cc27f62bd2c725a9
Christophe Lachance <lachance.christophe@cgmatane.qc.ca>
2019-09-24 10:16:34 -0400

    Debut de la fusion avec la branche de Antoine

---
+++ app/src/main/java/ca/qc/cgmatane/gestionrdv/vue/CalendrierAccueil.java
@@ -0,0 +1,61 @@
+package ca.qc.cgmatane.gestionrdv.vue;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import ca.qc.cgmatane.gestionrdv.R;
+
+public class CalendrierAccueil extends AppCompatActivity {
+    static final public int ACTIVITE_LISTE_EVENEMENT_PAR_JOUR = 1;
+    static final public String DATE_FORMAT = "yyyy/MM/dd";
+
+
+    private String dateChoisit;
+    private CalendarView vueCalendrierActionChoixJour;
+
+    protected Intent intentionNaviguerListeEvenementParJour;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_calendrier_accueil);
+
+        vueCalendrierActionChoixJour =(CalendarView) findViewById(R.id.vue_calendrier_action_choix_jour);
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        dateChoisit= formatter.format(date);
+
+        vueCalendrierActionChoixJour.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                dateChoisit = ""+i+"/"+i1+"/"+i2;
+            }
+        });
+
+        intentionNaviguerListeEvenementParJour = new Intent(this,ListeEvenementParJour.class);
+
+        Button vueCalendrierActionNaviguerListeEvenementParJour;
+        vueCalendrierActionNaviguerListeEvenementParJour =
+            (Button) findViewById(R.id.vue_calendrier_action_naviguer_liste_evenement_par_jour);
+
+        vueCalendrierActionNaviguerListeEvenementParJour.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    intentionNaviguerListeEvenementParJour.putExtra("date",dateChoisit);
+                    startActivityForResult(intentionNaviguerListeEvenementParJour,CalendrierAccueil.ACTIVITE_LISTE_EVENEMENT_PAR_JOUR);
+                }
+            }
+        );
+    }
+}
---
+++ app/src/main/java/ca/qc/cgmatane/gestionrdv/vue/ListeEvenementParJour.java
@@ -0,0 +1,24 @@
+package ca.qc.cgmatane.gestionrdv.vue;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ca.qc.cgmatane.gestionrdv.R;
+
+public class ListeEvenementParJour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_liste_evenement_par_jour);
+
+        Bundle parametres = this.getIntent().getExtras();
+        String dateChoisit = (String) parametres.get("date");
+
+        TextView titre = (TextView) findViewById(R.id.textView);
+
+        titre.setText("Evenememts le "+dateChoisit);
+    }
+}
