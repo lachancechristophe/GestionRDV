3fc407ae144612c5e526fe3bfde9c8a86ebdc04d
Unknown <lachance.christophe@cgmatane.qc.ca>
2019-09-24 11:01:51 -0400

    ListeEvenementsParJour
    Debut du code

--- app/src/main/java/ca/qc/cgmatane/gestionrdv/vue/ListeEvenementParJour.java
+++ app/src/main/java/ca/qc/cgmatane/gestionrdv/vue/ListeEvenementParJour.java
@@ -1,13 +1,38 @@
 package ca.qc.cgmatane.gestionrdv.vue;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.HashMap;
+
 import ca.qc.cgmatane.gestionrdv.R;
+import ca.qc.cgmatane.gestionrdv.modele.EvenementDAO;
 
 public class ListeEvenementParJour extends AppCompatActivity {
+
+    public static final int ACTIVITE_AJOUTER_EVENEMENT = 1;
+    public static final int ACTIVITE_MODIFIER_EVENEMENT = 2;
+
+    public static EvenementDAO baseDeDonnee;
+
+    protected ListView vueBibliothequeListeEvenements;
+
+    protected Intent intentionNaviguerAjouterEvenement;
+    protected Intent intentionNaviguerModifierEvenement;
+    protected Intent intentionNaviguerAlerte;
+
+    protected EvenementDAO accesseurEvenement;
+
+    /* TODO Gérer les alarmes
+    protected AlarmManager gestionnaireAlertes;
+    protected HashMap<Integer, PendingIntent> listeIntentionsLatentesAlertes;
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +42,7 @@ public class ListeEvenementParJour extends AppCompatActivity {
         Bundle parametres = this.getIntent().getExtras();
         String dateChoisit = (String) parametres.get("date");
 
-        TextView titre = (TextView) findViewById(R.id.textView);
+        TextView titre = (TextView) findViewById(R.id.vue_liste_evenements_texte_heure_choisie);
 
         titre.setText("Evenements le "+dateChoisit);
     }
