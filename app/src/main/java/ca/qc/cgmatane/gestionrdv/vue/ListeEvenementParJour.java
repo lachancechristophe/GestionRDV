5dc04b9eff06285c5fa318829d191efadae686a3
Unknown <lachance.christophe@cgmatane.qc.ca>
2019-09-24 11:22:25 -0400

    ListeEvenements
    Debut AfficherEvenementsDuJour(Date jour)

--- app/src/main/java/ca/qc/cgmatane/gestionrdv/vue/ListeEvenementParJour.java
+++ app/src/main/java/ca/qc/cgmatane/gestionrdv/vue/ListeEvenementParJour.java
@@ -1,14 +1,15 @@
 package ca.qc.cgmatane.gestionrdv.vue;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import ca.qc.cgmatane.gestionrdv.R;
@@ -21,7 +22,8 @@ public class ListeEvenementParJour extends AppCompatActivity {
 
     public static EvenementDAO baseDeDonnee;
 
-    protected ListView vueBibliothequeListeEvenements;
+    protected ListView vueListeEvenementsListeEvenements;
+    protected TextView vueListeEvenementsTexteDateChoisie;
 
     protected Intent intentionNaviguerAjouterEvenement;
     protected Intent intentionNaviguerModifierEvenement;
@@ -40,10 +42,29 @@ public class ListeEvenementParJour extends AppCompatActivity {
         setContentView(R.layout.vue_liste_evenement_par_jour);
 
         Bundle parametres = this.getIntent().getExtras();
-        String dateChoisit = (String) parametres.get("date");
+        String dateChoisie = (String) parametres.get("date");
 
-        TextView titre = (TextView) findViewById(R.id.vue_liste_evenements_texte_heure_choisie);
+        vueListeEvenementsTexteDateChoisie = (TextView) findViewById(R.id.vue_liste_evenements_texte_heure_choisie);
+        vueListeEvenementsListeEvenements = (ListView)findViewById(R.id.vue_liste_evenements_liste_affichage);
 
-        titre.setText("Evenements le "+dateChoisit);
+        vueListeEvenementsTexteDateChoisie.setText("Evenements le " + dateChoisie);
+        // TODO
+        //afficherEvenementsDuJour();
+    }
+
+    protected void afficherEvenementsDuJour(Date jourChoisi){
+
+        //TODO Get les events et les transformer en arraylist de hashmap string string
+        ArrayList<HashMap<String,String>> listeEvenements = new ArrayList<HashMap<String,String>>(); //accesseurEvenement.getTousEvenements();
+
+        SimpleAdapter adapteurVueBibliothequeListeEvenementss = new SimpleAdapter(
+                this,
+                listeEvenements,
+                android.R.layout.two_line_list_item,
+                // http://stackoverflow.com/questions/3663745/what-is-android-r-layout-simple-list-item-1
+                new String[] {"TitreAuteur", "Lire avant"}, // l'indice utilisé dans les hashmap
+                new int[] {android.R.id.text1, android.R.id.text2});
+
+        vueListeEvenementsListeEvenements.setAdapter(adapteurVueBibliothequeListeEvenementss);
     }
 }
